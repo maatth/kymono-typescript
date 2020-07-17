ed87094d6beb370a21143e773dcff71df44fdf80
Maat <maatthieu@gmail.com>
2020-07-17 11:32:51 +0200

    install react typescript

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import createrRouter from './routes/createrRouter'
 var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
-// var cors = require('cors')
+var cors = require('cors')
 const router = createrRouter()
 export let orm: MikroORM
 
@@ -18,7 +18,7 @@ async function startServer() {
 
   app.use(logger('dev'))
   app.use(express.json())
-  // app.use(cors())
+  app.use(cors())
   app.use(express.urlencoded({ extended: false }))
   app.use(cookieParser())
   app.use(express.static(path.join(__dirname, 'public')))
